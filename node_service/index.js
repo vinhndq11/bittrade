46d6cf7a7ae13874c5f18259023efe4d18e8f649
vinhndq <nguyenduyquangvinh@gmail.com>
2023-12-10 01:27:50 +0700

    update

--- node_service/index.js
+++ node_service/index.js
@@ -1,30 +1,35 @@
-const http = require('http');
-const socketio = require('socket.io');
-const redis = require('redis');
+const socket = require('./src/socket');
+const redisClient = require('./src/redis');
 const { tryParse, getTradeRate } = require('./src/helpers');
-const { ADMIN } = require('./src/config');
+const httpServer = require('http').createServer;
+const { Server } = require('socket.io');
 
-const port = process.env.PORT || 3000;
+const serverOptions = {
+    pingInterval: 10000,
+    pingTimeout: 5000,
+    cors: false,
+    cookie: true
+};
 
-const server = http.createServer();
-const io = socketio(server);
+const io = new Server(httpServer(), serverOptions);
 
-const redisClient = redis.createClient();
-
-io.on('connection', socket => {
-    socket.on('we_message', data => {
-        const { bet, get, path } = data;
-        const subscribers = tryParse(data.subscribers);
-        const cookie = tryParse(data.cookie);
-
-        // Your logic here
-    });
-
-    socket.on('disconnect', () => {
-        // Handle disconnect event
+io.on('connection', (socket) => {
+    console.log('Connected:', socket.id);
+    redisClient.on('message', function (channel, message) {
+        io.to(socket.id).emit(channel, message);
     });
 });
 
-server.listen(port, () => {
-    console.log(`Server is running at: http://localhost:${port}`);
+const socketPort = parseInt(process.env.SOCKET_PORT || '3000');
+
+httpServer.listen(socketPort, () => {
+    console.log('Server listening at port:', socketPort);
+    redisClient.on('error', function (err) {
+        console.error('Redis error:', err);
+    });
+});
+
+getTradeRate().then((tradeRate) => {
+    const betInstance = new Bet(io, tradeRate);
+    betInstance.start();
 });
