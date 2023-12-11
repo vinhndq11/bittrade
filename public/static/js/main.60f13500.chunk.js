2e91a73cbbba5bcafe2638e1d885e7a1569d49b6
vinhndq <nguyenduyquangvinh@gmail.com>
2023-12-11 17:37:45 +0700

    update

--- public/static/js/main.60f13500.chunk.js
+++ public/static/js/main.60f13500.chunk.js
@@ -7377,7 +7377,7 @@
                                             children: Object(M.jsxs)("p", {
                                                 "data-v-f99ecb42": "",
                                                 className: "colorGray bottomText",
-                                                children: [" Chưa có tài khoản BeeTradeX?", Object(M.jsx)("a", {
+                                                children: [" Chưa có tà khoản BeeTradeX?", Object(M.jsx)("a", {
                                                     "data-v-f99ecb42": "",
                                                     href: "#",
                                                     onClick: Z("register").bind(this),
